eb66d2a6d53e1062eb35160da6bee7dbe91e5881
teles <josetelesmaciel@gmail.com>
2017-09-28 00:27:47 -0300

    fix(Generated content): #1 - Fix duplication of original content

--- src/link-builder.js
+++ src/link-builder.js
@@ -1,17 +1,19 @@
-function LinkBuilder(content, replacements){    
-    return replacements.map(replacement => {
+function LinkBuilder(content, replacements){
+
+    replacements.forEach(replacement => {
         let counter = 0;
         let patternModifiers = replacement.insensitive ? "gi" : "g";
         let pattern = new RegExp(replacement.keyword, patternModifiers);
-            return content.replace(pattern, (match) =>{
-                let hasNoMax = typeof replacement.max === "undefined";
-                if(replacement.max > counter || hasNoMax) {
-                    counter++;
-                    return `<a href="${replacement.anchor}">${match}</a>`;
-                }
+        content = content.replace(pattern, (match) =>{
+            let hasNoMax = typeof replacement.max === "undefined";
+            if(replacement.max > counter || hasNoMax) {
+                counter++;
+                return `<a href="${replacement.anchor}">${match}</a>`;
+            }
             return match;
         });
     });
+    return content;
 }
 
 module.exports = LinkBuilder;
