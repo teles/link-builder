b9d6fa3d2f8786ff8b6b6e689fd798d4a907afc9
Jota Teles <josetelesmaciel@gmail.com>
2017-10-01 10:26:11 -0300

    closes #3 - include boundaries on replacement keyword

--- src/link-builder.js
+++ src/link-builder.js
@@ -3,7 +3,7 @@ function LinkBuilder(content, replacements){
     replacements.forEach(replacement => {
         let counter = 0;
         let patternModifiers = replacement.insensitive ? "gi" : "g";
-        let pattern = new RegExp(replacement.keyword, patternModifiers);
+        let pattern = new RegExp(`\\b${replacement.keyword}\\b`, patternModifiers);
         content = content.replace(pattern, (match) =>{
             let hasNoMax = typeof replacement.max === "undefined";
             if(replacement.max > counter || hasNoMax) {
